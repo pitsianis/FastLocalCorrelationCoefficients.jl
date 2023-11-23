922a70fc8e1a0d751dcb78e690e3677558164938
Theodoros Tyrovouzis <teotyrov@gmail.com>
2023-11-23 20:43:31 +0200

    Add python interface

---
+++ FastLocalCorrelationCoefficients/__init__.py
@@ -0,0 +1,17 @@
+
+from juliacall import Main as jl
+jl.seval("using FastLocalCorrelationCoefficients")
+
+from array import array
+
+
+def flcc(haystack: array, needle: array) -> array:
+    return jl.flcc(haystack, needle)
+
+
+def best_correlated(c: array):
+    return jl.best_correlated(c) - 1 # julia uses 1-indexing
+
+
+def lcc(haystack: array, needle: array) -> array:
+    return jl.lcc(haystack, needle)
